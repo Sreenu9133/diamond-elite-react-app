04e820b57fe661013306f34701f016ae6e756479
Srinivas Rao <kumarsrinivas945@gmail.com>
2026-07-17 07:37:31 +0530

    placed favicon

--- frontend/src/components/navbar/navbar.jsx
+++ frontend/src/components/navbar/navbar.jsx
@@ -8,7 +8,7 @@ function Navbar() {
         <>
             <nav className="flex items-center px-10 py-4 w-full justify-between shadow-2xl border-b border-emerald-950 bg-white max-[576px]:p-5">
 
-                <div className="logo flex items-center gap-1">
+                <div className="logo flex items-center gap-1 w-[20%]">
                     <IoDiamondOutline size={30} className="text-emerald-900" />
                     <h1 className="text-emerald-900 text-xl font-bold max-[576px]:text-sm">Diamond Elite</h1>
                 </div>
